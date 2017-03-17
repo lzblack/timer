e733e00299d7084c9274353810c17990f41e74ac
Zhi Li <zli@babson.edu>
2017-03-16 22:47:45 -0400

    modify timer.js

--- static/js/timer.js
+++ static/js/timer.js
@@ -3,7 +3,7 @@
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     var refresh = setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
+        minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
